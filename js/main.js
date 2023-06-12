0883f20a7c62cecf602331d201d611f98342faf6
Sami Armando Muongueno Sabino <samisabino@gmail.com>
2023-06-12 17:39:52 +0100

    Service Screen Responsive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 let menu = document.querySelector("#menu-icon");
+let iconService = document.querySelector("#menu-iconService");
 let navlist = document.querySelector(".navlist");
+let navlist2 = document.querySelector(".navlist2");
+
+let eyeOn = document.querySelector("#eyeOn");
+let eyeOff = document.querySelector("#eyeOff");
+
+eyeOn.style.display = 'none';
 
 menu.onclick = () => {
-
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
+
+iconService.onclick = () => {
+
+    alert('Click');
+    iconService.classList.toggle('bx-x');
+    navlist2.classList.toggle('open');
+}
+
+eyeOn.onclick = () => {
+
+    eyeOn.style.display = 'none';
+    eyeOff.style.display = 'block';
+}
+
+eyeOff.onclick = () => {
+
+    eyeOn.style.display = 'block';
+    eyeOff.style.display = 'none';}
 
 const sr = ScrollReveal({
 
